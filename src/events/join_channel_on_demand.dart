15d6dab1d083ae2c58483e0167de07861f969440
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-04-19 00:47:40 +0200

    feat: Add move

--- src/events/join_channel_on_demand.dart
+++ src/events/join_channel_on_demand.dart
@@ -7,7 +7,7 @@ import 'package:mineral/core/events.dart';
 class JoinChannelOnDemand extends MineralEvent<VoiceJoinEvent> with MineralContext {
   Future<void> handle (event) async {
     if (event.after.id == environment.get('CHANNEL_VOICE_ON_DEMAND')) {
-      await event.after.guild.channels.create(ChannelBuilder.fromVoiceChannel(
+      final channel = await event.after.guild.channels.create(ChannelBuilder.fromVoiceChannel(
         label: '${event.member.nickname} channel',
         permissions: [
           PermissionOverwrite(id: event.member.id, type: PermissionOverwriteType.role, deny: [ClientPermission.administrator]),
@@ -15,6 +15,10 @@ class JoinChannelOnDemand extends MineralEvent<VoiceJoinEvent> with MineralConte
         ],
         parentId: environment.get('CATEGORY_VOICE_ON_DEMAND')
       ));
+
+      if (channel != null) {
+        await event.member.voice.move(channel.id);
+      }
     }
   }
 }
